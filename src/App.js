e20a0b0c82c59d6eebc0f583fd89a74e9004666d
Raz Hoe <razifhoe@gmail.com>
2022-08-13 22:20:31 +0800

    Added QR code
    QR code is to ease transfer of content between one pc to another

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import QRCode from "react-qr-code";
+import { AiOutlineFileText, AiOutlineQrcode } from "react-icons/ai";
 
 function App() {
 	const [selecteddate, setSelecteddate] = useState(new Date().toISOString().split("T")[0]);
 	const [Achievement, setAchievement] = useState("");
+	const [showqr, setShowqr] = useState(false);
 
 	const updateAchievement = (event) => {
 		setAchievement(event.target.value);
 	}
 
 	useEffect(()=>{
-		if(localStorage.getItem(selecteddate)){
+
+		let params = new URLSearchParams(window.location.search);		
+		if(params.get('data')){
+			setAchievement(window.atob(params.get('data')));
+		}else if(localStorage.getItem(selecteddate)){
 			setAchievement(localStorage.getItem(selecteddate));
 		}
+
 	},[]);
 
 	useEffect(()=>{
@@ -38,16 +46,28 @@ function App() {
 					</div>
 
 					<div className="border-t border-gray-200 flex-auto h-full">
-						<textarea autoFocus className="px-4 py-5 sm:px-6 w-full h-full focus:outline-none caret-orange-400 text-gray-500 text-lg md:text-base resize-none" spellCheck="false" value={Achievement} onChange={updateAchievement} ></textarea>
+						<div className="flex h-full">
+							<div className="flex-auto h-full" id="section-input">
+								{showqr ? 
+									<div className="px-4 py-5 sm:px-6 text-center">
+										<QRCode className="mx-auto mb-5" value={window.location.origin+'/?data='+window.btoa(Achievement)} />
+										<small className="text-gray-400">{window.location.origin+'/?data='+window.btoa(Achievement)}</small>
+									</div>
+									:
+									<textarea autoFocus className="px-4 py-5 sm:px-6 w-full h-full focus:outline-none caret-orange-400 text-gray-500 text-lg md:text-base resize-none" spellCheck="false" value={Achievement} onChange={updateAchievement} ></textarea>
+								}
+							</div>
+						</div>
 					</div>
 
 					<div className="px-4 py-5 bg-gray-50 text-right sm:px-6 flex-none">
-						<small className="text-gray-400">Changes are automatically saved on your browser</small>
+						<div className="flex">
+							<small className="flex-auto text-left text-gray-400">Changes are automatically saved on your browser</small>
+							<button className='flex-none' onClick={()=>{ setShowqr(!showqr) }} title={!showqr ? 'Show Transfer QR' : 'Show Achievement'} >{!showqr ? <AiOutlineQrcode /> : <AiOutlineFileText /> }</button>
+						</div>
 					</div>
 
 				</div>
-
-
 
 			</div>
 		</div>
